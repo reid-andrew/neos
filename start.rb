9448e6335544d027bc7fb65e1e88efd2fff7fd74
Andrew Reid <areid.sj@gmail.com>
2020-04-27 10:59:07 -0600

    Reformat initial prompt in start file

--- start.rb
+++ start.rb
@@ -1,7 +1,8 @@
 require_relative 'near_earth_objects'
 
 puts "________________________________________________________________________________________________________________________________"
-puts "Welcome to NEO. Here you will find information about how many meteors, asteroids, comets pass by the earth every day. \nEnter a date below to get a list of the objects that have passed by the earth on that day."
+puts "Welcome to NEO. Here you will find information about how many meteors, asteroids, comets pass by the earth every day."
+puts "Enter a date below to get a list of the objects that have passed by the earth on that day."
 puts "Please enter a date in the following format YYYY-MM-DD."
 print ">>"
 
